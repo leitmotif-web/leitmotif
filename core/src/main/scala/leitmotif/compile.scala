64f1228011b49a4b7df5e6f11d531ad7a5801f98
Torsten Schmits <torstenschmits@gmail.com>
2018-11-21 21:53:13 +0100

    store arbitrary transitions as `List` in `Lm`

--- core/src/main/scala/leitmotif/compile.scala
+++ core/src/main/scala/leitmotif/compile.scala
@@ -6,17 +6,33 @@ import cats.implicits._
 
 object Compile
 {
-  def apply[A, B](context: Context[A, B]): Tree[Lm[A, B]] => Eval[(Context[A, B], Tree[Lm[A, B]])] = {
-    case Cofree(head, tail) =>
-      val (path, node) = head.pathTrans(context.path, head.node)
-      val context1 = context.copy(path = path)
-      for {
-        sub <- tail
-        (context2, updated) <- sub.foldM[Eval, (Context[A, B], List[Tree[Lm[A, B]]])]((context1, Nil)) {
-          case ((c, s0), a) => apply(c)(a).map {
-            case (c1, s1) => (c1, s0 :+ s1)
+  def trans[A, B]
+  (tail: Eval[List[Tree[Lm[A, B]]]])
+  (z: (Context[A, B], Tree[Lm[A, B]]), a: Trans[A, B])
+  : Eval[(Context[A, B], Tree[Lm[A, B]])] = {
+    val (context, tree) = z
+    a match {
+      case Trans.Rec() =>
+        for {
+          sub <- tail
+          (updatedContext, updatedSub) <- sub.foldM[Eval, (Context[A, B], List[Tree[Lm[A, B]]])]((context, Nil)) {
+            case ((c, s0), a) => apply(c)(a).map {
+              case (c1, s1) => (c1, s0 :+ s1)
+            }
           }
-        }
-      } yield (context2, Cofree(Lm[A, B](node), Eval.now(updated)))
+        } yield (updatedContext, Cofree(tree.head, Eval.now(updatedSub)))
+      case Trans.Path(f) =>
+        val (path, node) = f(context.path, tree.head.node)
+        val updatedContext = context.copy(path = path)
+        val updatedTree = tree.copy(head = tree.head.copy(node = node))
+        Eval.now((updatedContext, updatedTree))
+      case _ =>
+        ???
+    }
+  }
+
+  def apply[A, B](context: Context[A, B]): Tree[Lm[A, B]] => Eval[(Context[A, B], Tree[Lm[A, B]])] = {
+    case tree @ Cofree(head, tail) =>
+      head.trans.foldM((context, tree))(trans(tail))
   }
 }
--- core/src/main/scala/leitmotif/data.scala
+++ core/src/main/scala/leitmotif/data.scala
@@ -21,17 +21,31 @@ case class ContextPre()
 
 case class Context[A, B](path: A, sub: B)
 
-case class Lm[A, B](node: El, pathTrans: (A, El) => (A, El), subTrans: (B, El) => (B, El))
+sealed trait Trans[A, B]
+
+object Trans
+{
+  case class Rec[A, B]()
+  extends Trans[A, B]
+
+  case class Path[A, B](f: (A, El) => (A, El))
+  extends Trans[A, B]
+
+  case class Sub[A, B](f: (B, El) => (B, El))
+  extends Trans[A, B]
+}
+
+case class Lm[A, B](node: El, trans: List[Trans[A, B]])
 {
   def path(f: (A, El) => (A, El)): Lm[A, B] =
-    copy(pathTrans = f)
+    copy(trans = Trans.Path[A, B](f) :: trans)
 
   def sub(f: (B, El) => (B, El)): Lm[A, B] =
-    copy(subTrans = f)
+    copy(trans = trans :+ Trans.Sub[A, B](f))
 }
 
 object Lm
 {
   def apply[A, B](node: El): Lm[A, B] =
-    Lm(node, (a, e) => (a, e), (b, e) => (b, e))
+    Lm(node, List(Trans.Rec()))
 }
